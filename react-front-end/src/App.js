400f83439bf8c12bab29d1811000a126b5c5f576
Adam Hart <adamhayhart@gmail.com>
2021-06-01 14:20:08 -0700

    testt2 heroku

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -23,8 +23,8 @@ import SearchResults from "./component/SearchResults";
 import axios from "axios";
 
 import ErrorPage from "./component/404";
-import ReactDOM from "react-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
+// import ReactDOM from "react-dom";
+// import { Auth0Provider } from "@auth0/auth0-react";
 
 export const JobsContext = createContext([]);
 
