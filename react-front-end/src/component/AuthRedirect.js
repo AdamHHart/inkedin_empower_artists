e0f195427a4de542f0a736c41a9c46e8e404e253
Adam Hart <adamhayhart@gmail.com>
2021-08-27 12:23:55 -0700

    Deployed front end on Netlify

---
+++ react-front-end/src/component/AuthRedirect.js
@@ -0,0 +1,13 @@
+// // Configured to allow users to login using google
+// // Not configured with app functionality, would have to replace current method of login authentication
+
+// import React from "react";
+// import { useAuth0 } from "@auth0/auth0-react";
+
+// const LoginButton = () => {
+//   const { loginWithRedirect } = useAuth0();
+
+//   return <button onClick={() => loginWithRedirect()}>Log In</button>;
+// };
+
+// export default LoginButton;
